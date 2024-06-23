0c549bd1a13588acceedbc3e99dd4a391b8b2d55
kei <jaeykweon@naver.com>
2024-06-23 23:07:12 +0900

    project init

---
+++ src/main/kotlin/org/idd/webfluxcoroutinepractice/WebfluxCoroutinePracticeApplication.kt
@@ -0,0 +1,11 @@
+package org.idd.webfluxcoroutinepractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxCoroutinePracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxCoroutinePracticeApplication>(*args)
+}
---
+++ src/test/kotlin/org/idd/webfluxcoroutinepractice/WebfluxCoroutinePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.idd.webfluxcoroutinepractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxCoroutinePracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
