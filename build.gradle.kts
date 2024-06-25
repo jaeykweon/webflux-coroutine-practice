0e57929870beed7b78db891bcaddc4bcbb305a39
kei <jaeykweon@naver.com>
2024-06-25 13:48:25 +0900

    kotest 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    val kotestVersion = "5.9.1"
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 kotlin {
